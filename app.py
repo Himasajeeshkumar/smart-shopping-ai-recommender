c6046cf12c664c4fe75eaa05ea788b86acca00e3
Himasajeeshkumar <juug23btech15532@gmail.com>
2025-04-05 20:15:35 +0530

    Updated app.py with Render deployment support

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 from recomender import recommend_products
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', recommended_products=None)
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
-    customer_id = request.form['customer_id']
-    recommendations = recommend_products(customer_id)
-    return render_template('index.html', recommendations=recommendations)
+    customer_id = request.form.get('customer_id')
+    if customer_id:
+        recommended_products = recommend_products(customer_id)
+        if recommended_products:
+            return render_template('index.html', recommended_products=recommended_products, customer_id=customer_id)
+        else:
+            return render_template('index.html', recommended_products=[], customer_id=customer_id, message="Customer ID not found.")
+    else:
+        return render_template('index.html', recommended_products=[], message="Please enter a Customer ID.")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
